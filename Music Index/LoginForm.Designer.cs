cfbc6e9449e2df8f68cebe83581ffc3b1fe725f8
rhiensch <info@robhiensch.nl>
2013-01-23 00:38:04 +0100

    hoppaa

---
+++ Music Index/LoginForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Music_Index
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.btLogin = new System.Windows.Forms.Button();
+            this.btRegistreer = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btWWVergeten = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btLogin
+            // 
+            this.btLogin.Location = new System.Drawing.Point(80, 149);
+            this.btLogin.Name = "btLogin";
+            this.btLogin.Size = new System.Drawing.Size(75, 23);
+            this.btLogin.TabIndex = 0;
+            this.btLogin.Text = "Login";
+            this.btLogin.UseVisualStyleBackColor = true;
+            // 
+            // btRegistreer
+            // 
+            this.btRegistreer.Location = new System.Drawing.Point(161, 149);
+            this.btRegistreer.Name = "btRegistreer";
+            this.btRegistreer.Size = new System.Drawing.Size(75, 23);
+            this.btRegistreer.TabIndex = 1;
+            this.btRegistreer.Text = "Registreer";
+            this.btRegistreer.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(352, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(352, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // btWWVergeten
+            // 
+            this.btWWVergeten.Location = new System.Drawing.Point(242, 149);
+            this.btWWVergeten.Name = "btWWVergeten";
+            this.btWWVergeten.Size = new System.Drawing.Size(123, 23);
+            this.btWWVergeten.TabIndex = 5;
+            this.btWWVergeten.Text = "Wachtwoord vergeten";
+            this.btWWVergeten.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 184);
+            this.Controls.Add(this.btWWVergeten);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btRegistreer);
+            this.Controls.Add(this.btLogin);
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button btLogin;
+        private System.Windows.Forms.Button btRegistreer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btWWVergeten;
+    }
+}
+
